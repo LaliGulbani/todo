734e1c3ac884e4b9e6f9f0af600e96deea1b8d44
Lali Hulbani <Lali_Hulbani@epam.com>
2019-10-23 19:22:59 +0300

    Create structure for Todo app.

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import AppHeader from './app-header';
+import SearchPanel from './search-panel';
+import TodoList from './todo-list';
+import ItemStatusFilter from './item-status-filter';
+import App from './app';
+
+export { TodoList, SearchPanel, AppHeader, ItemStatusFilter, App };
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    return (
+        <input text="text"
+            className="form-control search-input"
+            placeholder="my input"/>
+    );
+};
+
+export default SearchPanel;
